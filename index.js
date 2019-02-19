5b6346fcaad8a1aa1c485c8bf08dbe84cc1f7500
huya.nzb <huya.nzb@taobao.com>
2019-02-19 17:54:27 +0800

    bugfix

--- index.js
+++ index.js
@@ -1,47 +1,69 @@
 const { ConcatSource } = require("webpack-sources");
 const validateOptions = require("schema-utils");
-const schema = require('./schema.json');
+const schema = require("./schema.json");
 
 class BabelExternalHelpersWebpackPlugin {
   constructor(options) {
     this.options = options || {};
-    validateOptions(schema, this.options, 'BabelExternalHelpersWebpackPlugin');
+    validateOptions(schema, this.options, "BabelExternalHelpersWebpackPlugin");
   }
 
   apply(compiler) {
-    let {
-      outputType = 'global',
-      whitelist = 'auto',
-      babel
-    } = this.options;
+    const _thisOptions = this.options;
 
-    if (typeof babel === 'string') {
-      babel = require(babel);
+    if (typeof _thisOptions.babel === "string") {
+      _thisOptions.babel = require(_thisOptions.babel);
     }
 
-		compiler.hooks.compilation.tap("BabelExternalHelpersWebpackPlugin", compilation => {
-			compilation.hooks.optimizeChunkAssets.tap("BabelExternalHelpersWebpackPlugin", chunks => {
-				for (const chunk of chunks) {
-					if (!chunk.canBeInitial()) {
-						continue;
-					}
+    compiler.hooks.compilation.tap(
+      "BabelExternalHelpersWebpackPlugin",
+      compilation => {
+        compilation.hooks.optimizeChunkAssets.tap(
+          "BabelExternalHelpersWebpackPlugin",
+          chunks => {
+            for (const chunk of chunks) {
+              if (!chunk.canBeInitial()) continue;
 
-					for (const file of chunk.files) {
-            if (whitelist === 'auto') {
-              const code = compilation.assets[file].source();
-              const helpers = code.match(/babelHelpers\.(\w+)/g);
-              whitelist = helpers && helpers.length ? 
-                helpers.filter((m, i) => helpers.indexOf(m) === i).map(m => m.split('.')[1]) : null;
+              for (const file of chunk.files) {
+                if (!/\.js$/.test(file)) continue;
+                let whitelist = _thisOptions.whitelist || "auto";
+                let outputType = _thisOptions.outputType || "global";
+                if (whitelist === "auto") {
+                  const code = compilation.assets[file].source();
+                  const helpers = code.match(/babelHelpers\.(\w+)/g);
+                  whitelist =
+                    helpers && helpers.length
+                      ? helpers
+                          .filter((m, i) => helpers.indexOf(m) === i)
+                          .map(m => m.split(".")[1])
+                      : [];
+                }
+                let helpersCode = _thisOptions.babel.buildExternalHelpers(
+                  whitelist,
+                  outputType
+                );
+                if (outputType === "global") {
+                  helpersCode = helpersCode.replace(
+                    /global\.babelHelpers\s*=\s*\{\}/i,
+                    "global.babelHelpers = (global.babelHelpers || {})"
+                  );
+                } else if (outputType === "var") {
+                  helpersCode = helpersCode.replace(
+                    /var\s+babelHelpers\s*=\s*\{\}/i,
+                    'var babelHelpers = typeof babelHelpers === "undefined" ? {} : babelHelpers'
+                  );
+                }
+                compilation.assets[file] = new ConcatSource(
+                  helpersCode,
+                  "\n\n",
+                  compilation.assets[file]
+                );
+              }
             }
-						compilation.assets[file] = new ConcatSource(
-							babel.buildExternalHelpers(whitelist, outputType),
-							"\n\n",
-							compilation.assets[file]
-						);
-					}
-				}
-			});
-		});
+          }
+        );
+      }
+    );
   }
 }
 
